4fa1e4ad8b968e3bdc039da34fff14d3edfadbc6
AtNovember <atnovemb@gmail.com>
2017-03-02 13:46:19 +0400

    sending random voice

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
 const TelegramBot = require('node-telegram-bot-api');
-// const fs = require('fs');
-
-// replace the value below with the Telegram token you receive from @BotFather
+const fs = require('fs');
 const token = '344155317:AAGKB6zS-mJ31WQi1roHSv-EPYgaG2ZRIdo';
-
-// Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
+const sounds = require('./sound.js');
 
-// Matches "/echo [whatever]"
 // бот реагирует на команду /echo
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
   const chatId = msg.chat.id;
   const resp = match[1]; // the captured "whatever"
   console.log('resp', resp);
-
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message.
-// There are different kinds of messages.
 // бот реагирует на сообщение
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Received your message');
   console.log('Received your message', msg.text);
-  bot.sendVoice(chatId, 'sound/terran/MARINE/tmayes00.wav');
+
+    var index = Math.floor(Math.random() * (sounds.sounds.length - 0)) + 0;
+    var randomVoice = sounds.sounds[index];
+
+    const buffer = fs.readFileSync(randomVoice);
+    bot.sendVoice(chatId, buffer);
 });
